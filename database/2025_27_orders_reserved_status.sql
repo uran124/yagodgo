5abeab846ca4cad7fbfebed20ac687b52c066550
uran124 <55823948+uran124@users.noreply.github.com>
2026-05-28 18:20:45 +0700

    Delete database/2025_27_orders_reserved_status.sql

--- database/2025_27_orders_reserved_status.sql
+++
@@ -1,5 +0,0 @@
--- Добавляем статус "reserved" для заказов-броней
-ALTER TABLE orders
-    MODIFY COLUMN status ENUM('reserved','new','processing','assigned','delivered','cancelled')
-    NOT NULL DEFAULT 'new';
-
