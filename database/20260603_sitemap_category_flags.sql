e642a9b4dead69699b316f4ca9dc7cad204a9691
uran124 <55823948+uran124@users.noreply.github.com>
2026-07-14 10:39:56 +0700

    Delete database/20260603_sitemap_category_flags.sql

--- database/20260603_sitemap_category_flags.sql
+++
@@ -1,2 +0,0 @@
-CALL yg_add_column_if_missing('product_types', '`in_sitemap` tinyint(1) NOT NULL DEFAULT 1');
-CALL yg_add_column_if_missing('content_categories', '`in_sitemap` tinyint(1) NOT NULL DEFAULT 1');
